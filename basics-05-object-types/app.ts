628b108f5e4612db105edebd217e0aa38f7d9696
narapark7561 <narapark7561@gmail.com>
2024-10-27 22:37:21 -0600

    added code

--- basics-05-object-types/app.ts
+++ basics-05-object-types/app.ts
@@ -1,10 +1,18 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-const person = {
+// // const person: {
+// //   name: string;
+// //   age: number;
+// // } = {
+// const person = {
+//   name: 'Maximilian',
+//   age: 30,
+// };
+
+const person: {
+  name: string;
+  age: 30;
+} = {
   name: 'Maximilian',
-  age: 30
+  age: 30,
 };
 
 console.log(person.name);
