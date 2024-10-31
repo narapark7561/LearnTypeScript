6d124632170cfa7532e3f7badc8e0d65d8aec50f
narapark7561 <narapark7561@gmail.com>
2024-10-30 20:05:11 -0600

    learning array type

--- basics-05-object-types/app.ts
+++ basics-05-object-types/app.ts
@@ -1,18 +1,28 @@
-// // const person: {
-// //   name: string;
-// //   age: number;
-// // } = {
-// const person = {
-//   name: 'Maximilian',
-//   age: 30,
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: 'Chai',
+//   age: 7,
 // };
 
-const person: {
-  name: string;
-  age: 30;
-} = {
-  name: 'Maximilian',
-  age: 30,
+// console.log(person.name);
+
+const person = {
+  name: 'Chai',
+  age: 7,
+  hobbies: ['eat', 'play', 'sleep'],
 };
 
-console.log(person.name);
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+  console.log(hobby.toUpperCase());
+  console.log(hobby.map()); //map자체는 array에 쓸수있는데 여기서 string[]이라고 타입지정을 해도, for 문을 돌면 string으로인식해서 map을쓸수없다.
+}
+
+let favoriteActivities: string[];
+// favoriteActivities = 'run'; -> getting error becuase type is array
+
+favoriteActivities = ['Running'];
+
+console.log(favoriteActivities);
