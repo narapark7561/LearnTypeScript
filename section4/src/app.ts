14b9845f8c05e9aa67c4dce1088eb48b82eb1c0e
narapark7561 <narapark7561@gmail.com>
2024-11-22 23:45:33 -0700

    Sperad Operator ( ... )

--- section4/src/app.ts
+++ section4/src/app.ts
@@ -1,18 +1,14 @@
-//default functon  parameters
+// Speard Operator (...)
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking'];
 
-const add = (a: number, b: number = 1) => a + b; //giving default parameter
+activeHobbies.push(...hobbies);
 
-const printOutput: (a: number | string) => void = (output) =>
-  console.log(output);
-const button = document.querySelector('button');
+const person = {
+  name: 'Max',
+  age: 30,
+};
 
-if (button) {
-  button.addEventListener('click', (event) => console.log(event));
-}
+const copiedPerson = { ...person };
 
-printOutput(add(5));
-
-//이렇게하면 에러가 안난다. 5를 자동으로 첫번째 parameter로인식하기떄문.
-// 그렇지만
-//const add = (a: number = 1, b: number) => a + b;
-//이렇게하면 에러가 난다. 왜? 기본 매개변수를 갖는 매개변수는 항상 마지막에 있어야 하기 때문
+console.log(copiedPerson);
