f92cd883cbcb19eb51b3015be66dbe5236aa0129
narapark7561 <narapark7561@gmail.com>
2024-12-12 19:36:13 -0700

    interface extends with another interface

---
+++ section5_interface/src/interfaces_extends.ts
@@ -0,0 +1,25 @@
+interface Named {
+  readonly name: string;
+}
+
+interface Greetable extends Named {
+  greet(phrase: string): void;
+}
+
+class Person implements Greetable, Named {
+  name: string;
+  age = 30;
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  greet(phrase: string) {
+    console.log(phrase + ' ' + this.name);
+  }
+}
+
+let user1: Greetable;
+user1 = new Person('Max'); //constructor 때문에 이게 name으로 들어간다.
+
+user1.greet('Hi, there! I am');
+console.log(user1);
