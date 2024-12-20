f155d45c609442258cec9770443b0a57fd4d1b79
narapark7561 <narapark7561@gmail.com>
2024-12-20 16:49:57 -0700

    keyof Constraint

--- section7_generics/src/app.ts
+++ section7_generics/src/app.ts
@@ -8,3 +8,33 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergeObj = merge({ name: 'Max' }, { age: 30 });
 console.log(mergeObj.age);
+
+// another generic function
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let decriptionText = 'Got no value';
+  if (element.length === 1) {
+    decriptionText = 'Got 1 elements';
+  } else if (element.length > 1) {
+    decriptionText = 'Got ' + element.length + ' elements';
+  }
+  return [element, decriptionText];
+}
+
+console.log(countAndDescribe('Hi there!'));
+console.log(countAndDescribe(['Sports', 'Cooking']));
+
+// keyof contraints
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return 'Value' + obj[key];
+}
+
+extractAndConvert({ name: 'Max' }, 'name');
