32dc93f03efd070b424a5f46c8aa47ad8e6d4749
narapark7561 <narapark7561@gmail.com>
2024-12-20 19:54:28 -0700

    Starting Section 9 - Decorators

---
+++ section7_generics/src/Generic_Class.ts
@@ -0,0 +1,23 @@
+class DataStorage<T> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
